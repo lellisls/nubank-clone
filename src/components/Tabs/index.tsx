d214c62cc14ddad031f67fb93545ccc33d156502
Lucas Lellis <lucaslellis777@gmail.com>
2019-09-23 23:03:22 -0300

    More tabs

--- src/components/Tabs/index.tsx
+++ src/components/Tabs/index.tsx
@@ -11,6 +11,22 @@ export default function Tabs() {
                 <Icon name="person-add" size={24} color="#FFF" />
                 <TabText>Indicar Amigos</TabText>
             </TabItem>
+            <TabItem>
+                <Icon name="chat-bubble-outline" size={24} color="#FFF" />
+                <TabText>Cobrar</TabText>
+            </TabItem>
+            <TabItem>
+                <Icon name="arrow-downward" size={24} color="#FFF" />
+                <TabText>Depositar</TabText>
+            </TabItem>
+            <TabItem>
+                <Icon name="arrow-upward" size={24} color="#FFF" />
+                <TabText>Transferir</TabText>
+            </TabItem>
+            <TabItem>
+                <Icon name="lock" size={24} color="#FFF" />
+                <TabText>Bloquear Cartão</TabText>
+            </TabItem>
         </TabsContainer>
     </Container>
   );
