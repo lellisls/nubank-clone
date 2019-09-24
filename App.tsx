bcaf9d2af2336af6fd4bcb505d2bbc0d527ce042
Lucas Lellis <lucaslellis777@gmail.com>
2019-09-23 22:20:34 -0300

    Initial setup + statusbar + Main page

--- App.tsx
+++ App.tsx
@@ -1,19 +1,12 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import Main from "./src/pages/Main";
+import { StatusBar } from "react-native";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-    </View>
-  );
-}
+const App = () => (
+  <>
+    <StatusBar barStyle="light-content" backgroundColor="#8B10AE" />
+    <Main />
+  </>
+);
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
