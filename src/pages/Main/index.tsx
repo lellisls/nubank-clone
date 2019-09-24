9669237b9bc1430b524656484d2dd9d83f222b62
Lucas Lellis <lucaslellis777@gmail.com>
2019-09-23 23:33:20 -0300

    Card from main screen

--- src/pages/Main/index.tsx
+++ src/pages/Main/index.tsx
@@ -1,12 +1,41 @@
 import React from "react";
-import { Container } from "./styles";
+import {
+  Container,
+  Content,
+  Card,
+  CardHeader,
+  CardContent,
+  Title,
+  Description,
+  CardFooter,
+  Annotation
+} from "./styles";
 import Header from "../../components/Header";
 import Tabs from "../../components/Tabs";
+
+import Icon from "react-native-vector-icons/MaterialIcons";
 
 export default function Main() {
   return (
     <Container>
       <Header />
+      <Content>
+        <Card>
+          <CardHeader>
+            <Icon name="attach-money" size={28} color="#666"></Icon>
+            <Icon name="visibility-off" size={28} color="#666"></Icon>
+          </CardHeader>
+          <CardContent>
+            <Title>Saldo Disponível</Title>
+            <Description>R$ 2.453.654,06</Description>
+          </CardContent>
+          <CardFooter>
+            <Annotation>
+              Transferência de R$ 2.453.460,00 recebida de Loteria Federal
+            </Annotation>
+          </CardFooter>
+        </Card>
+      </Content>
       <Tabs></Tabs>
     </Container>
   );
