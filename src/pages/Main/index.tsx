bbd630a5ee4115e3a0ec3226c3d076784f3c8b00
Lucas Lellis <lucaslellis777@gmail.com>
2019-09-23 22:47:02 -0300

    Application Header

--- src/pages/Main/index.tsx
+++ src/pages/Main/index.tsx
@@ -1,6 +1,11 @@
 import React from "react";
 import { Container } from "./styles";
+import Header from "../../components/Header";
 
 export default function Main() {
-  return <Container></Container>;
+  return (
+    <Container>
+      <Header />
+    </Container>
+  );
 }
